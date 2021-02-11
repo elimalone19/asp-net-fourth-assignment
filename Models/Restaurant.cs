da269c0b8c3b70925b156396405d01ed60c37df7
elimalone19 <elimalone19@gmail.com>
2021-02-10 19:37:29 -0700

    initial commit

---
+++ Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_net_fourth_assignment.Models
+{
+    public class Restaurant
+    {
+        public int RestRating { get; set; }
+        public string RestName { get; set; }
+        public float RestPoints { get; set; }
+
+
+    }
+}
